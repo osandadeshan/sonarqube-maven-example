128bcd1c56b611aeb71bfbc6c1e4afc054b75d37
Osanda Deshan <osanda.deshan@gmail.com>
2019-08-10 18:12:09 +0530

    Initial commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,22 @@
+/**
+ * Project Name    : sonarqube-example
+ * Developer       : Osanda Deshan
+ * Version         : 1.0.0
+ * Date            : 8/8/2019
+ * Time            : 4:28 PM
+ * Description     :
+ **/
+
+
+public class HelloWorld {
+
+    public void sayHello() {
+        System.out.println("Hello World!");
+    }
+
+    public void notCovered() {
+        System.out.println("This method is not covered by unit tests");
+    }
+
+
+}
---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+
+/**
+ * Project Name    : sonarqube-example
+ * Developer       : Osanda Deshan
+ * Version         : 1.0.0
+ * Date            : 8/9/2019
+ * Time            : 8:59 AM
+ * Description     :
+ **/
+
+
+public class HelloWorldTest {
+
+    @Test
+    public void sayHello() {
+        new HelloWorld().sayHello();
+    }
+
+
+}
